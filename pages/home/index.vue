f9709196a282e349fe289bc0fb052c31509ea315
liuchunfeng <liuchunfeng@jd.com>
2019-05-31 11:19:07 +0800

    update 调整首页样式

---
+++ pages/home/index.vue
@@ -0,0 +1,166 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#001529"
+        text-color="#fff"
+        active-text-color="#1890ff"
+      >
+        <el-submenu index="2">
+          <template slot="title">机房管理</template>
+          <el-menu-item index="2-1">待处理工单</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="2-4">
+            <template slot="title">选项5</template>
+            <el-menu-item index="2-4-1">选项51</el-menu-item>
+            <el-menu-item index="2-4-2">选项51</el-menu-item>
+            <el-menu-item index="2-4-3">选项53</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+
+        <el-submenu index="1">
+          <template slot="title">资源平台</template>
+
+          <el-submenu>
+            <template slot="title">设备管理</template>
+            <el-menu-item>机柜管理</el-menu-item>
+            <el-menu-item>机房列表</el-menu-item>
+          </el-submenu>
+
+          <el-submenu>
+            <template slot="title">资源管理</template>
+            <el-menu-item>资源分配</el-menu-item>
+            <el-menu-item>资源回收</el-menu-item>
+            <el-menu-item>配置修改</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-header>
+
+    <el-main>
+      <el-row :gutter="10">
+        <el-col :span="8">{{menuData}}</el-col>
+        <el-col :span="8">{{menuData}}</el-col>
+        <el-col :span="8">{{menuData}}</el-col>
+      </el-row>
+    </el-main>
+
+    <el-footer>
+      <el-row>
+        <el-col :span="8">
+          <ul>
+            <li>
+              <a href="#">百度</a>
+            </li>
+            <li>
+              <a href="#">百度</a>
+            </li>
+            <li>
+              <a href="#">百度</a>
+            </li>
+          </ul>
+        </el-col>
+        <el-col :span="8">
+          <ul>
+            <li>
+              <a href="#">百度</a>
+            </li>
+            <li>
+              <a href="#">百度</a>
+            </li>
+            <li>
+              <a href="#">百度</a>
+            </li>
+          </ul>
+        </el-col>
+        <el-col :span="8">
+          <ul>
+            <li>
+              <a href="#">百度</a>
+            </li>
+            <li>
+              <a href="#">百度</a>
+            </li>
+            <li>
+              <a href="#">百度</a>
+            </li>
+          </ul>
+        </el-col>
+      </el-row>
+      <!-- -->
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import menuData from "./menu";
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  },
+  computed: {
+    menuData() {
+      return menuData;
+    }
+  }
+};
+</script>
+<style scoped>
+.el-header {
+  background: #001529;
+}
+
+.el-footer {
+  color: #fff;
+  background-color: #001529;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  height: initial !important;
+}
+
+.el-footer ul {
+  /* display: flex; */
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  margin-left: 20px;
+}
+
+.el-footer ul li {
+  /* flex: 1; */
+  padding: 0;
+  /* text-align: center; */
+  margin: 0;
+  line-height: 30px;
+}
+
+.el-footer a {
+  text-decoration: none;
+  color: #fff;
+  transition: color 0.3s;
+}
+.el-footer a:hover {
+  color: #1890ff;
+}
+</style>
---
+++ pages/home/menu.js
@@ -0,0 +1,299 @@
+export default {
+  "menuId": "1",
+  "menuName": "",
+  "menuTitle": "资源平台",
+  "menuUrl": "",
+  "children": [
+    {
+      "menuId": "2",
+      "menuName": "serverManageGroup",
+      "menuTitle": "设备管理",
+      "menuUrl": "",
+      "children": [
+        {
+          "menuId": "365",
+          "menuName": "cabinetCapacity",
+          "menuTitle": "机柜管理",
+          "menuUrl": "/IdcUI/#/cabinet/cabinetCapacity",
+          "children": [
+            {
+              "menuId": "358",
+              "menuName": "cabinetlist",
+              "menuTitle": "机柜列表",
+              "menuUrl": "/IdcUI/#/cabinetlist",
+              "operation": [
+
+              ]
+            }
+          ],
+          "operation": [
+
+          ]
+        },
+        {
+          "menuId": "3",
+          "menuName": "servermanage",
+          "menuTitle": "设备列表",
+          "menuUrl": "/IdcUI/#/servermanage",
+          "children": [
+            {
+              "menuId": "117",
+              "menuName": "serverdetail",
+              "menuTitle": "设备详情",
+              "menuUrl": "/IdcUI/#/serverdetail/:sn",
+              "operation": [
+
+              ]
+            }
+          ],
+          "operation": [
+
+          ]
+        }
+      ],
+      "operation": [
+
+      ]
+    },
+    {
+      "menuId": "5",
+      "menuName": "resourceManageGroup",
+      "menuTitle": "资源管理",
+      "menuUrl": "",
+      "children": [
+        {
+          "menuId": "6",
+          "menuName": "#",
+          "menuTitle": "资源分配",
+          "menuUrl": "/IdcOp/index.php?m=Home&c=Distribute&a=companyResources",
+          "operation": [
+
+          ]
+        },
+        {
+          "menuId": "7",
+          "menuName": "#",
+          "menuTitle": "资源回收",
+          "menuUrl": "/IdcOp/index.php?m=Home&c=ServerRecycle&a=recycle",
+          "operation": [
+
+          ]
+        },
+        {
+          "menuId": "8",
+          "menuName": "#",
+          "menuTitle": "配置修改",
+          "menuUrl": "/IdcOp/index.php?m=Home&c=Config&a=index",
+          "operation": [
+
+          ]
+        }
+      ],
+      "operation": [
+
+      ]
+    },
+    {
+      "menuId": "9",
+      "menuName": "fixedAssetMangeGroup",
+      "menuTitle": "固资管理",
+      "menuUrl": "",
+      "children": [
+        {
+          "menuId": "10",
+          "menuName": "#",
+          "menuTitle": "入库验收",
+          "menuUrl": "/SupplyChain/index.php?m=Ysrk&c=Index&a=delivery",
+          "operation": [
+
+          ]
+        },
+        {
+          "menuId": "12",
+          "menuName": "#",
+          "menuTitle": "数据字典",
+          "menuUrl": "/IdcOp/index.php?m=Home&c=Dict&a=relation",
+          "operation": [
+
+          ]
+        }
+      ],
+      "operation": [
+
+      ]
+    },
+    {
+      "menuId": "87",
+      "menuName": "packageMangeGroup",
+      "menuTitle": "套餐字典",
+      "menuUrl": "#",
+      "children": [
+        {
+          "menuId": "88",
+          "menuName": "package_list",
+          "menuTitle": "套餐列表",
+          "menuUrl": "/IdcUI/#/package/list",
+          "operation": [
+
+          ]
+        },
+        {
+          "menuId": "119",
+          "menuName": "package_setting",
+          "menuTitle": "新增套餐",
+          "menuUrl": "/IdcUI/#/package/setting/0",
+          "operation": [
+
+          ]
+        }
+      ],
+      "operation": [
+
+      ]
+    },
+    {
+      "menuId": "186",
+      "menuName": "cmdbGroup",
+      "menuTitle": "新版字典",
+      "menuUrl": "/IdcUI/#/cmdb",
+      "children": [
+        {
+          "menuId": "176",
+          "menuName": "cmdb",
+          "menuTitle": "数据字典",
+          "menuUrl": "/IdcUI/#/cmdb",
+          "children": [
+            {
+              "menuId": "256",
+              "menuName": "accessory-number",
+              "menuTitle": "配件号",
+              "menuUrl": "/IdcUI/#/cmdb/accessory-number",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "177",
+              "menuName": "cmdb_partType",
+              "menuTitle": "配件类型列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-partType",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "178",
+              "menuName": "cmdb_typeAttr",
+              "menuTitle": "类型属性列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-typeAttr",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "179",
+              "menuName": "cmdb_partAttr",
+              "menuTitle": "配件属性列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-partAttr",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "180",
+              "menuName": "cmdb_brand",
+              "menuTitle": "品牌列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-brand",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "181",
+              "menuName": "cmdb_supplier",
+              "menuTitle": "供应商列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-supplier",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "182",
+              "menuName": "cmdb_deviceType",
+              "menuTitle": "设备类型列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-deviceType",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "183",
+              "menuName": "cmdb_unitType",
+              "menuTitle": "设备型号列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-unitType",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "184",
+              "menuName": "cmdb_partSpecifi",
+              "menuTitle": "配件规格列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-partSpecifi",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "185",
+              "menuName": "cmdb_partVersion",
+              "menuTitle": "配件型号列表",
+              "menuUrl": "/IdcUI/#/cmdb/cmdb-partVersion",
+              "operation": [
+
+              ]
+            }
+          ],
+          "operation": [
+
+          ]
+        },
+        {
+          "menuId": "253",
+          "menuName": "accessory",
+          "menuTitle": "配件管理",
+          "menuUrl": "/IdcUI/#/accessory",
+          "children": [
+            {
+              "menuId": "254",
+              "menuName": "accessory-instock",
+              "menuTitle": "配件库存",
+              "menuUrl": "/IdcUI/#/accessory/accessory-instock",
+              "operation": [
+
+              ]
+            },
+            {
+              "menuId": "255",
+              "menuName": "accessory-detail",
+              "menuTitle": "库存明细",
+              "menuUrl": "/IdcUI/#/accessory/accessory-detail",
+              "operation": [
+
+              ]
+            }
+          ],
+          "operation": [
+
+          ]
+        }
+      ],
+      "operation": [
+
+      ]
+    }
+  ],
+  "operation": [
+
+  ]
+}
